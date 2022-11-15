3122cbc1f253d470238db9105858748012b12bd0
Singrum <103807515+singrum@users.noreply.github.com>
2022-11-15 22:28:22 +0900

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -1,25 +1,72 @@
+
+
+let camera, scene, renderer;
+
+
+//GUI생성
+let controls = new function(){
+    this.rotationSpeed = 0.02;
+    this.bouncingSpeed = 0.03;
+}
+let gui = new dat.GUI();
+gui.add(controls, "rotationSpeed", 0, 0.5);
+gui.add(controls, "bouncingSpeed", 0, 0.5);
+
+
 function setting(){
-    let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
     
-    let renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0xEEEEEE);
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.shadowMap.enabled = true;
     let axes = new THREE.AxesHelper(20);
     scene.add(axes);
-    return [scene, camera, renderer]
-}
-function renderScene(){
-    // cube.rotation.x += 0.02;
-    // cube.rotation.y += 0.02;
-    // cube.rotation.z += 0.02;
-    requestAnimationFrame(renderScene);
-    renderer.render(scene, camera);
 }
 
+
 function init(){
-    [scene, camera, renderer] = setting();
+    //화면 리사이즈 맞춤
+    window.addEventListener("resize", onResize, false);
+    function onResize(){
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+
+    //회전 애니메이션
+    let i =0;
+    function renderScene_rotate(){
+        
+        cube.rotation.x += controls.rotationSpeed;
+        cube.rotation.y += controls.rotationSpeed;
+        cube.rotation.z += controls.rotationSpeed;
+        requestAnimationFrame(renderScene_rotate);
+        renderer.render(scene, camera);
+    }
+    //바운스 애니메이션
+    function renderScene_bounce(){
+        i += 0.4;
+        cube.position.x = 20 + (10 * (Math.cos(i)));
+        cube.position.y = 2 + (10 * Math.abs(Math.sin(i)));
+        requestAnimationFrame(renderScene_bounce);
+        renderer.render(scene, camera);
+    }
+    function renderScene_both(){
+        
+        cube.rotation.x += controls.rotationSpeed;
+        cube.rotation.y += controls.rotationSpeed;
+        cube.rotation.z += controls.rotationSpeed;
+        i += controls.bouncingSpeed;
+        cube.position.x = 20 + (10 * (Math.cos(i)));
+        cube.position.y = 2 + (10 * Math.abs(Math.sin(i)));
+        requestAnimationFrame(renderScene_both);
+        renderer.render(scene, camera);
+    }
+
+    setting();
 
     let planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
     let planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
@@ -36,8 +83,8 @@ function init(){
     let cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
     let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     
-    cube.position.x = 0;
-    cube.position.y = 2;
+    cube.position.x = -6;
+    cube.position.y = 6;
     cube.position.z = 0;
     cube.castShadow = true;
     scene.add(cube)
@@ -54,6 +101,6 @@ function init(){
     scene.add(spotLight);
 
     document.body.appendChild(renderer.domElement);
-    renderScene();
+    renderScene_both();
 }
 window.onload = init;
